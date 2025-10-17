35f7c13354a0409a5835c762eeb017b56ff6ae74
Erick-ken-tamae <erickkentamae@gmail.com>
2025-10-17 18:22:05 -0300

    aula js

--- projeto/frontend/js/script.js
+++ projeto/frontend/js/script.js
@@ -1,20 +1,44 @@
-const protocolo = "http://";        //protocolo
-const baseURL =  "localhost:3000";  //porta:3000
-const filmEndpoint = "/filmes";    
+const protocolo = "http://";
+const baseURL = "localhost:3000";
+const filmesEndpoint = "/filmes";
 
-async function obtemFilmes(){
-    const URLCompleta = `${protocolo}${baseURL}${filmEndpoint}`;
-    const filmes = (await axios.get(URLCompleta)).data;
-    //console.log(filmes)
-    let tabela = document.querySelector('.filmes');
-    let corpoTabela = tabela.getElementsByTagName('tbody')[0];
-    //para cada filme, criar uma nova linha 
-    for(let filme of filmes ){
-        //a inserção da linha será no início, poderia ser no fim (sem argumento)
-        let linha = corpoTabela.insertRow(0)
-        let celulaTitulo = linha.insertCell(0)
-        let celulaSinopse = linha.insertCell(1)
-        celulaTitulo.innerHTML = filme.titulo
-        celulaSinopse.innerHTML = filme.sinopse 
-    }
- }
+async function obtemFilmes() {
+  const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+  const filmes = (await axios.get(URLcompleta)).data;
+  //console.log(filmes)
+  let tabela = document.querySelector(".filmes");
+  let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+  //para cada filme, criar uma nova linha
+  for (let filme of filmes) {
+    //a inserção da linha será no início, poderia ser no fim (sem argumentos no insertRow)
+    let linha = corpoTabela.insertRow(0);
+    let celulaTitulo = linha.insertCell(0);
+    let celulaSinopse = linha.insertCell(1);
+    celulaTitulo.innerHTML = filme.titulo;
+    celulaSinopse.innerHTML = filme.sinopse;
+  }
+}
+async function cadastrarFilme() {
+  //montamos a URL completa
+  const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`;
+  //pega os inputs digitados pelo usuário
+  let tituloInput = document.querySelector("#tituloInput");
+  let sinopseInput = document.querySelector("#sinopseInput");
+  let titulo = tituloInput.value;
+  let sinopse = sinopseInput.value;
+  //limpa as caixinhas de input
+  tituloInput.value = "";
+  sinopseInput.value = "";
+  const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data;
+  //limpar a tabela e preencher com a lista nova de filmes
+  let tabela = document.querySelector(".filmes");
+  let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+  corpoTabela.innerHTML = "";
+  for (let filme of filmes) {
+    let linha = corpoTabela.insertRow(0);
+    let celulaTitulo = linha.insertCell(0);
+    let celulaSinopse = linha.insertCell(1);
+    celulaTitulo.innerHTML = filme.titulo;
+    celulaSinopse.innerHTML = filme.sinopse;
+  }
+}
--- projeto/index.js
+++ projeto/index.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const cors = require("cors");
+const express = require("express");
+const cors = require("cors")
 const app = express();
 app.use(express.json());
 app.use(cors())
-//REQUISISÃO GET no endereço http://localhost:3000/oi
-app.get('/oi', (req, res) => {
-    res.send('OI')
-})
+
+//requisição GET no endereço http://localhost:3000/oi
+app.get("/oi", (req, res) => {
+  res.send("oi");
+});
 
 let filmes = [
-    {
-        titulo: "Forrest Gump - O Contador de Histórias",
-        sinopse: "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks),um rapaz com QI abaixo da média e boas intenções."
-    },
-    {
-        titulo: "Um Sonho de Liberdade",
-        sinopse: "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime quenunca cometeu, o homicídio de sua esposa e do amantedela"
-    },
-    {
-        "titulo": "Interestelar",
-        "sinopse": "As reservas naturais da Terra estão chegando ao fim e um grupo de astronautas recebe a missão de verificar possíveis planetas para receberem a população mundial, possibilitando a continuação da espécie. Cooper é chamado para liderar o grupo e aceita a missão sabendo que pode nunca mais ver os filhos. Ao lado de Brand, Jenkins e Doyle, ele seguirá em busca de um novo lar"
-    },
+  {
+    titulo: "Forrest Gump - O Contador de Histórias",
+    sinopse:
+      "Quarenta anos da história dos Estados Unidos, vistos pelos olhos de Forrest Gump (Tom Hanks), um rapaz com QI abaixo da média e boas intenções.",
+  },
+  {
+    titulo: "Um Sonho de Liberdade",
+    sinopse:
+      "Em 1946, Andy Dufresne (Tim Robbins), um jovem e bem sucedido banqueiro, tem a sua vida radicalmente modificada ao ser condenado por um crime que nunca cometeu, o homicídio de sua esposa e do amante dela",
+  },
 ];
-//requisição GET para obter lsiat de filmes http://localhost:3000/filmes
+
+//requisição GET para obter a lista de filmes: http://localhost:3000/filmes
 app.get("/filmes", (req, res) => {
     res.json(filmes)
 })
 
-// REQUISÃO PARA CADASTRAR UM NOVO FILME NAA MEMÓRIA
-//POST http://localhost:3000/filmes
+//requisição para cadastrar um novo filme NAAA MEMÓÓÓRIA
+//post http://localhost:3000/filmes
 app.post("/filmes", (req, res) => {
-    //obtem as informações que chegam 
-    const titulo = req.body.titulo
-    const sinopse =  req.body.sinopse
-    //monta o objeto json
-    const filme = {titulo: titulo, sinopse: sinopse}
-    filmes.push(filme)
-    //só para verificar
-    res.send(filmes)
+  //obtem as informações que chegam
+  const titulo = req.body.titulo
+  const sinopse = req.body.sinopse
+  //monta o objeto json
+  const filme = {titulo: titulo, sinopse: sinopse}
+  filmes.push(filme)
+  //só para verificar
+  res.send(filmes)
 })
 
-app.listen(3000, () => console.log(" SERVIDOR up and running"))
+app.listen(3000, () => console.log("servidor up and running"));
