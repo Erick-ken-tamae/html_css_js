0dbc6ad34aa51db1f284acd36a7e1aa0b7590d44
ERICK KEN TAMAE <25.00240-3@maua.br>
2025-10-21 11:04:56 -0300

    mongo-sql

--- projeto/frontend/js/script.js
+++ projeto/frontend/js/script.js
@@ -26,19 +26,31 @@ async function cadastrarFilme() {
   let sinopseInput = document.querySelector("#sinopseInput");
   let titulo = tituloInput.value;
   let sinopse = sinopseInput.value;
-  //limpa as caixinhas de input
-  tituloInput.value = "";
-  sinopseInput.value = "";
-  const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data;
-  //limpar a tabela e preencher com a lista nova de filmes
-  let tabela = document.querySelector(".filmes");
-  let corpoTabela = tabela.getElementsByTagName("tbody")[0];
-  corpoTabela.innerHTML = "";
-  for (let filme of filmes) {
-    let linha = corpoTabela.insertRow(0);
-    let celulaTitulo = linha.insertCell(0);
-    let celulaSinopse = linha.insertCell(1);
-    celulaTitulo.innerHTML = filme.titulo;
-    celulaSinopse.innerHTML = filme.sinopse;
+  if (titulo && sinopse) {
+    //limpa as caixinhas de input
+    tituloInput.value = "";
+    sinopseInput.value = "";
+    const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data;
+    //limpar a tabela e preencher com a lista nova de filmes
+    let tabela = document.querySelector(".filmes");
+    let corpoTabela = tabela.getElementsByTagName("tbody")[0];
+    corpoTabela.innerHTML = "";
+    for (let filme of filmes) {
+      let linha = corpoTabela.insertRow(0);
+      let celulaTitulo = linha.insertCell(0);
+      let celulaSinopse = linha.insertCell(1);
+      celulaTitulo.innerHTML = filme.titulo;
+      celulaSinopse.innerHTML = filme.sinopse;
+    }
+  }
+  //senão, exibe o alerta por até 2 segundos
+  else{
+    let alert = document.querySelector('.alert')
+    alert.classList.add('show')
+    alert.classList.remove('d-none')
+    setTimeout(() =>{
+      alert.classList.remove('show')
+      alert.classList.add('d-none')
+    }, 2000)
   }
 }
